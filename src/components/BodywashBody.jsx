d9dc2cc4b4f79b2791a6fca4f2581cff9631bb28
Ryan <rphillips0071@gmail.com>
2024-05-29 12:44:09 -0400

    Dynamically import images to avoid build renaming issue

--- src/components/BodywashBody.jsx
+++ src/components/BodywashBody.jsx
@@ -1,14 +1,7 @@
 import React, { useContext } from 'react';
 import Products from '../assets/products.json';
 import { ShoppingCartContext } from '../context/ShoppingCartContext';
-
-function importAll(r) {
-  let images = {};
-  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-  return images;
-}
-
-const images = importAll(require.context('../assets', false, /\.(png|jpe?g|svg)$/));
+import image1 from '../assets/1.jpg';
 
 function BodywashBody() {
     const { addToCart, cartItems } = useContext(ShoppingCartContext);
@@ -19,15 +12,16 @@ function BodywashBody() {
             <div className="product_container">
                 {Products.filter(product => product.type === 'body wash').map(product => {
                     const cartItemsAmount = cartItems[product.id]; 
-
+                    
                     return (
                         <div className="product_card" key={product.id}>
-                            <img src={images[product.image]} alt={product.name} className="product_image" />
+                            <img src={product.image} alt={product.name} className="product_image" />
                             <h2 className="product_name">{product.name}</h2>
                             <p className="product_price">${product.price.toFixed(2)}</p>
                             <button className="product_button" onClick={() => addToCart(product.id)}>
                                 Add to Bag {cartItemsAmount > 0 && <>({cartItemsAmount})</>}
                             </button>
+
                         </div>
                     );
                 })}
