84cf4136025b9a2d0bb4353a8fa10f65668a757c
Ryan <rphillips0071@gmail.com>
2024-05-29 11:05:26 -0400

    Manually import assets and update App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,7 @@ import Bodywash from './pages/Bodywash';
 import Handsoap from './pages/Handsoap';
 import Cart from './pages/Cart';
 import { ShoppingCartContextProvider } from './context/ShoppingCartContext';
-import img1 from './assets/1.jpg';
-import img2 from './assets/2.jpg';
-import img3 from './assets/3.jpg';
-import img4 from './assets/4.jpg';
-import img5 from './assets/5.jpg';
+
 
 function App() {
   return (
